0cd91b372e0cc9c5943bcb17614122d23b21ece0
liran <liranusa6@gmail.com>
2021-06-27 16:59:51 +0300

    sender finish mix finish *without threading & round reciver finish with threading

--- reciver.py
+++ reciver.py
@@ -10,15 +10,24 @@ from cryptography.hazmat.primitives import hashes
 
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
+
 def decrypt_message(message):
     return Fernet(decryption_key).decrypt(message)
+
+
+def analyzing_message(sock):
+    data = client_socket.recv(8192)
+    content = decrypt_message(data).decode()
+    t = time.localtime()
+    current_time = str(t[3]) + ":" + str(t[4]) + ":" + str(t[5])
+    print(content + " " + current_time)
 
 
 if len(sys.argv) < 3:
     print("Wrong amount of arguments. should be two.")
     exit(-1)
 
-#create the decryption key
+# create the decryption key
 password = bytes(sys.argv[1].encode())
 salt = bytes(sys.argv[2].encode())
 kdf = PBKDF2HMAC(
@@ -29,7 +38,6 @@ kdf = PBKDF2HMAC(
 )
 decryption_key = base64.urlsafe_b64encode(kdf.derive(password))
 
-
 print("please enter listening port: ")
 port = int(input())
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -39,12 +47,14 @@ s.listen()
 
 while True:
     client_socket, client_address = s.accept()
-    data = client_socket.recv(8192)
-    content = decrypt_message(data).decode()
-    t= time.localtime()
-    current_time = str(t[3])+":"+str(t[4])+":"+str(t[5])
-    print(content+ " " +current_time)
-    print( "yupi")
+    thread = threading.Thread(target=analyzing_message, args=(client_socket,))
+    thread.start()
+    # data = client_socket.recv(8192)
+    # content = decrypt_message(data).decode()
+    # t= time.localtime()
+    # current_time = str(t[3])+":"+str(t[4])+":"+str(t[5])
+    # print(content+ " " +current_time)
+    print("yupi")
 #
 
 # t = f.decrypt(data)
